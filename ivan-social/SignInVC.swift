e0c45f302714fb88adedd2f73aab57faa88aeba5
Ivan Slavov <ivan.krasimirov.slavov86@gmail.com@ivans-imac.local>
2016-10-01 20:19:51 +0300

    test

--- ivan-social/SignInVC.swift
+++ ivan-social/SignInVC.swift
@@ -8,10 +8,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class SignInVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("test")
         // Do any additional setup after loading the view, typically from a nib.
     }
 
