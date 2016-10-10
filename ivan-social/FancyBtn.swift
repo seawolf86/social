8fa88f01d25b897ab264c377da4c3518b511f5e8
Ivan Slavov <ivan.krasimirov.slavov86@gmail.com@Ivans-iMac.local>
2016-10-10 20:49:05 +0300

    coments

--- ivan-social/FancyBtn.swift
+++ ivan-social/FancyBtn.swift
@@ -14,9 +14,9 @@ class FancyBtn: UIButton {
         super.awakeFromNib()
         
         layer.shadowColor = UIColor(red: SHADOW_GRAY, green: SHADOW_GRAY, blue: SHADOW_GRAY, alpha: 0.6).cgColor
-        layer.shadowOpacity = 0.8
-        layer.shadowRadius = 5.0
-        layer.shadowOffset = CGSize(width: 1.0, height: 1.0)
+        layer.shadowOpacity = 0.8 //sets how transparent the shadow is, where 0 is invisible and 1 is as strong as possible.
+        layer.shadowRadius = 5.0 ////sets how wide the shadow should be.
+        layer.shadowOffset = CGSize(width: 1.0, height: 1.0)  //sets how far away from the view the shadow should be, to give a 3D offset effect.
         layer.cornerRadius = 2.0
     }
 
