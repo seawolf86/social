73f85b238324d6a0a1be2afaf31691c8eab8bceb
Ivan Slavov <ivan.krasimirov.slavov86@gmail.com@Ivans-iMac.local>
2016-10-15 17:09:20 +0300

    facebook integration

--- Pods/Target Support Files/Pods-ivan-social/Pods-ivan-social-frameworks.sh
+++ Pods/Target Support Files/Pods-ivan-social/Pods-ivan-social-frameworks.sh
@@ -82,3 +82,14 @@ strip_invalid_archs() {
   fi
 }
 
+
+if [[ "$CONFIGURATION" == "Debug" ]]; then
+  install_framework "Pods-ivan-social/Bolts.framework"
+  install_framework "Pods-ivan-social/FBSDKCoreKit.framework"
+  install_framework "Pods-ivan-social/FBSDKLoginKit.framework"
+fi
+if [[ "$CONFIGURATION" == "Release" ]]; then
+  install_framework "Pods-ivan-social/Bolts.framework"
+  install_framework "Pods-ivan-social/FBSDKCoreKit.framework"
+  install_framework "Pods-ivan-social/FBSDKLoginKit.framework"
+fi
